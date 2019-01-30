415e0652f613121cbcc3d5ffab18f1c05266874a
Ben Swift <ben@benswift.me>
2019-01-30 17:44:48 +1100

    add custom hook to print bibtex output for livecoding gigs

--- _plugins/hooks.rb
+++ _plugins/hooks.rb
@@ -20,9 +20,37 @@ def write_lc_set_md_files()
   end
 end
 
+# custom bib output
+require "bibtex"
+
+def gig_to_bib(gig)
+
+  gig_meta = gig.data
+  authors = gig_meta["artists"].join(" and ")
+  date = gig_meta['date']
+
+  # return the bib entry data structure
+  BibTeX::Entry.new(
+    {
+      :bibtex_type => :misc,
+      :title => gig_meta['event'],
+      :author => authors,
+      :editor => gig_meta['curator'],
+      :howpublished => gig_meta['venue'],
+      :keywords => "livecoding, new media art",
+      :publisher => 'The Pragmatic Bookshelf',
+      :day => date.strftime('%d'),
+      :month => date.strftime('%b'),
+      :year => date.strftime('%Y')
+    })
+end
+
+def write_livecoding_bibliography(gigs)
+  bib = BibTeX::Bibliography.new
+  bib = gigs.map { |gig| gig_to_bib gig }
+  puts bib
+end
+
 Jekyll::Hooks.register :site, :post_render do |site|
-  # write_lc_set_md_files
-  # site.collections["livecoding"].docs.each { |page|
-  #   puts "(\"#{page['event']}\"\"#{page['venue']}\"\"#{page['date']}\")"
-  # }
+  write_livecoding_bibliography site.collections["livecoding"].docs
 end
