27ee7a0abf5c458a7d6991ff2032bf6a83d8bd1b
Ben Swift <ben@benswift.me>
2019-02-21 11:33:12 +1100

    tweak cljs

--- _cljs/reimagine_logo/src/reimagine_logo/core.cljs
+++ _cljs/reimagine_logo/src/reimagine_logo/core.cljs
@@ -1,12 +1,9 @@
 (ns reimagine-logo.core
-  (:require [clojure.string :as string]
-            [reimagine-logo.dom-helpers :as dom]))
+  (:require [reimagine-logo.dom-helpers :as dom]))
 
 (defn mount-logo []
   (->> "REIMAGINE"
-       (map
-        (fn [c]
-          (dom/element :div {:style (if (= c "A") "letter-A" "letter")} c)))
+       (map #(dom/element :div {:style (if (= % "A") "letter-A" "letter")} %))
        (apply dom/append (dom/get-element :logo))))
 
 (mount-logo)
