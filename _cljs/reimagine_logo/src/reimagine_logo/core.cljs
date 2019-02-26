29bf5d5216ab454efbb5a0a25eae28d612d77446
Ben Swift <ben@benswift.me>
2019-02-26 16:29:59 +1100

    react adding random classes works ok

--- _cljs/reimagine_logo/src/reimagine_logo/core.cljs
+++ _cljs/reimagine_logo/src/reimagine_logo/core.cljs
@@ -4,13 +4,19 @@
 (defn letter-component [letter]
   [:div letter])
 
+(defn random-classes []
+  (->> ["big" "small" "heavy" "light" "cw90" "cc90" "cw180" "cc180" "cw270" "cc270"]
+      shuffle
+      (take (rand-int 3))))
+
 (defn logo-component []
   [:div {:id "logo"}
    (for [letter "REIMAGINE"]
-     ^{:key letter} [:div {:class "letter"} letter])])
+     ^{:key letter} [:div {:class (concat ["letter" (when (= letter "A") "letter-A")] (random-classes))} letter])])
 
 (defn mountit []
   (r/render [logo-component]
             (js/document.getElementById "logo-container")))
 
 (mountit)
+
