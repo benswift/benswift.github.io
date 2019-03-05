7a4515c5dffef3fae0dcbca33b6583fa712df729
Ben Swift <ben@benswift.me>
2019-03-05 19:58:47 +1100

    more clojurian funtimes
    more idiomatic. now I feel smarter.

--- _cljs/reimagine_logo/src/reimagine_logo/core.cljs
+++ _cljs/reimagine_logo/src/reimagine_logo/core.cljs
@@ -1,18 +1,21 @@
 (ns reimagine-logo.core
   (:require [reagent.core :as r]))
 
-(defn letter-component [letter]
-  [:div letter])
-
 (defn random-classes []
   (->> ["big" "small" "heavy" "light" "cw90" "cc90" "cw180" "cc180" "cw270" "cc270"]
       shuffle
       (take (rand-int 3))))
 
+(defn letter-component [letter]
+  ^{:key letter}
+  [:div
+   {:class (concat ["letter" (when (= letter "A") "letter-A")] (random-classes))}
+   letter])
+
 (defn logo-component []
-  [:div {:id "logo"}
-   (for [letter "REIMAGINE"]
-     ^{:key letter} [:div {:class (concat ["letter" (when (= letter "A") "letter-A")] (random-classes))} letter])])
+  (into
+   [:div {:id "logo"}]
+   (map letter-component "REIMAGINE")))
 
 (defn mount []
   (r/render [logo-component]
