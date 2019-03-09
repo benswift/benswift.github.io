91b9b0a947c5ad3d6e9299e84c1fbd8a009912f1
Ben Swift <ben@benswift.me>
2019-03-09 16:02:42 +1100

    add cljs-building hook

---
+++ _plugins/cljs-build.rb
@@ -0,0 +1,17 @@
+# build all the clojurescript projects in _cljs/
+#
+# since they might use different build processes, it actually just calls make in
+# each subdirectory (so yeah, there's actually nothing cljs-specific about it)
+
+def build_cljs_projects(site)
+  Dir.foreach("_cljs") do |project_dir|
+    next if project_dir == '.' or project_dir == '..'
+    Dir.chdir("_cljs/#{project_dir}"){
+      system("make", "all") or raise "cljs build error in #{project_dir}"
+    }
+  end
+end
+
+Jekyll::Hooks.register :site, :pre_render do |site|
+  build_cljs_projects site
+end
