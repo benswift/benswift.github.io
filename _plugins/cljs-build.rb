7a1d44fb3004b65d06e97dd1b2b63bbc5bc0359b
Ben Swift <ben@benswift.me>
2019-03-13 10:04:15 +1100

    silence the output of the clojurescript make commands

--- _plugins/cljs-build.rb
+++ _plugins/cljs-build.rb
@@ -7,7 +7,7 @@ def build_cljs_projects(site)
   Dir.foreach("_cljs") do |project_dir|
     next if project_dir == '.' or project_dir == '..'
     Dir.chdir("_cljs/#{project_dir}"){
-      system("make", "all") or raise "cljs build error in #{project_dir}"
+      system("make", "--silent", "all") or raise "cljs build error in #{project_dir}"
     }
   end
 end
