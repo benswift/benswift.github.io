e57ac00b9ba1db021982753c04d0990c46b17b76
Ben Swift <ben@benswift.me>
2019-04-16 13:23:17 +1000

    WIP - updating to a more "do stuff in js" approach to animating the logo

--- _cljs/reimagine_logo/src/reimagine_logo/core.cljs
+++ _cljs/reimagine_logo/src/reimagine_logo/core.cljs
@@ -1,23 +1,36 @@
 (ns reimagine-logo.core
   (:require [reagent.core :as r]
+            [goog.string :as gstring]
+            [goog.string.format]
             [goog.dom.fullscreen :refer [requestFullScreen]]))
 
-(defn random-classes []
-  (->> ["big" "small" "heavy" "light" "cw90" "cc90" "cw180" "cc180" "cw270" "cc270"]
-       shuffle
-       (take (rand-int 3))))
+(defonce fps 60)
 
-(defn letter-component [letter]
-  ^{:key letter}
-  [:div
-   {:class (into ["letter" (when (= letter "A") "letter-A")]
-                 (random-classes))}
-   letter])
+(def letters
+  (r/atom (map #(assoc {:freq 0 :weight 400} :letter %)
+               "REIMAGINE")))
+
+(defn update-letter [letter-data]
+  (-> letter-data
+      (assoc :freq (rand 30))
+      (assoc :weight (+ 100 (rand 800)))))
+
+(defn letter-component [{:keys [letter freq weight]}]
+  (let [phase (r/atom 0)
+        ms-per-frame (/ 1000 fps)]
+    (fn []
+      (js/setTimeout #(swap! phase + (/ freq fps)) ms-per-frame)
+      ^{:key letter}
+      [:div.letter
+       {:style
+        {:transform (gstring/format "rotate(%.4frad)" @phase)
+         :font-weight weight}}
+       letter])))
 
 (defn logo-component []
   (into
    [:div {:id "logo"}]
-   (map letter-component "REIMAGINE")))
+   (map letter-component @letters)))
 
 (defn toggle-fullscreen []
   (requestFullScreen (js/document.getElementById "backdrop")))
@@ -28,7 +41,9 @@
 
 (defn main []
   (mount)
-  (js/setInterval #(mount) 10000)
+  (js/setInterval
+   (fn [] (swap! letters #(map update-letter %)))
+   10000)
   (js/document.addEventListener "keydown"
                                 #(when (= (.-code %) "KeyF")
                                    (toggle-fullscreen))))
