ecfdc29b5e290255136bc19d96cde072f7619a83
Ben Swift <ben@benswift.me>
2019-04-17 16:44:56 +1000

    bring everything into logo-component, still not re-rendering properly

--- _cljs/reimagine_logo/src/reimagine_logo/core.cljs
+++ _cljs/reimagine_logo/src/reimagine_logo/core.cljs
@@ -4,27 +4,15 @@
             [goog.string.format]
             [goog.dom.fullscreen :refer [requestFullScreen]]))
 
-(defonce fps 60)
-
-(def letters
-  (r/atom (map #(assoc {:rd 0 :wd 0} :letter %)
-               "REIMAGINE")))
-
-(defn update-letter-rates []
-  (let [update-letter #(merge % {:rd (- 0.1 (rand 0.1))
-                                 :wd (- 0.1 (rand 0.1))})]
-    (swap! letters #(map update-letter %))))
-
 (defn letter-component [{:keys [letter rd wd]}]
   (let [rot (r/atom 0)
         weight (r/atom 300)
-        ms-per-frame (/ 1000 fps)
         update-atoms (fn []
                        (swap! rot + rd)
                        (when (< 100 @weight 900)
                          (swap! weight + wd)))]
     (fn []
-      (js/setTimeout update-atoms ms-per-frame)
+      (js/setTimeout update-atoms (/ 1000 60)) ;; 60fps
       ^{:key letter}
       [:div.letter
        {:style
@@ -33,21 +21,34 @@
        letter])))
 
 (defn logo-component []
-  (into
-   [:div {:id "logo"}]
-   (for [letter-data @letters]
-     [letter-component letter-data])))
+  (let [letters
+        (r/atom (map #(assoc {:rd 0 :wd 0} :letter %)
+                     "REIMAGINE"))
+
+        update-letter
+        (fn [letter-data]
+          (-> letter-data
+              (assoc :rd (- 0.1 (rand 0.1)))
+              (assoc :wd (- 0.1 (rand 0.1)))))]
+    (fn []
+      (js/setTimeout
+       (fn []
+         (swap! letters #(map update-letter %)))
+       1000) ;; change spinnies every 10s
+      (into
+       [:div {:id "logo"}]
+       (for [letter-data @letters]
+         [letter-component letter-data])))))
 
 (defn toggle-fullscreen []
   (requestFullScreen (js/document.getElementById "backdrop")))
 
 (defn mount []
-  (r/render [logo-component @letters]
+  (r/render [logo-component]
             (js/document.getElementById "logo-container")))
 
 (defn main []
   (mount)
-  (js/setInterval update-letter-rates 1000)
   (js/document.addEventListener "keydown"
                                 #(when (= (.-code %) "KeyF")
                                    (toggle-fullscreen))))
