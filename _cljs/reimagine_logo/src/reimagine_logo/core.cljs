c14fedc3b74063d1f69886aa03ea84fb46ca8bc3
Ben Swift <ben@benswift.me>
2019-05-01 19:14:54 +1000

    updated RL renders again, but no animation

--- _cljs/reimagine_logo/src/reimagine_logo/core.cljs
+++ _cljs/reimagine_logo/src/reimagine_logo/core.cljs
@@ -1,58 +1,53 @@
 (ns reimagine-logo.core
-  (:require [reimagine-logo.tween :as tween]
+  (:require [reimagine-logo.tween :refer [ease-in-out transition-elastic]]
             [reagent.core :as r]
             [goog.string :as gstring]
             [goog.string.format]
             [goog.dom.fullscreen :refer [requestFullScreen]]))
 
-(defn letter-component [{:keys [letter ad sd wd]}]
-  (let [angle (r/atom (if (= letter "A") -90 0)) ;; deg
-        size (r/atom 10) ;; vmin
-        weight (r/atom 200)]
-    (fn [{:keys [letter ad sd wd]}]
-      (js/setTimeout
-       (fn []
-         (swap! angle + ad)
-         (when (< 3 @size 25) (swap! size + sd))
-         (when (< 100 @weight 900) (swap! weight + wd)))
-       (/ 1000 60)) ;; 60fps
+(defn rand-attribute-val [attribute]
+  (let [[min max]
+        (case attribute
+          :angle [-3600 3600]
+          :size [5 25]
+          :weight [100 900])])
+  (+ min (rand (- max min))))
+
+(defn attribute->style [attribute value]
+  (case attribute
+    :angle {:transform (gstring/format "rotate(%.2fdeg)" value)}
+    :size {:font-size (gstring/format "%.2fvmin" value)}
+    :weight {:font-weight value}))
+
+(defn easing-closure [attribute initial]
+  (let [final (rand-attribute-val attribute)
+        range (- final initial)
+        num-steps (rand-int 60)]
+    (fn [step]
+      (->> (/ step num-steps)
+           (ease-in-out transition-elastic)
+           (* range)
+           (+ initial)))))
+
+(defn letter-component [letter]
+  (let [step (r/atom 0)
+        style-transition-closures
+        (into {} (map (fn [attr] {attr (easing-closure attr (rand-attribute-val attr))}) [:angle :size :weight]))]
+    (fn [letter]
+      (js/setTimeout #(swap! step inc) (/ 1000 60)) ;; 60fps
       ^{:key letter}
       [:div.letter
        {:style
-        {:transform (gstring/format "rotate(%.2fdeg)" @angle)
-         :font-size (gstring/format "%.2fvmin" @size)
-         :font-weight @weight}}
+        (into {} (map (fn [[attr easing-fn]] {attr (attribute->style attr (easing-fn 0))})
+                      style-transition-closures))}
        letter])))
 
-(defn new-delta [attribute]
-  (if (< (rand 10) 7)
-    ;; most of the time, set the delta to zero so it won't change
-    0
-    ;; otherwise update the per-frame delta
-    (case attribute
-      :ad (- 1 (rand 2))
-      :sd (- 0.01 (rand 0.02))
-      :wd (- 1 (rand 2)))))
-
 (defn logo-component []
-  (let [letters
-        (r/atom (map #(assoc {:ad 0 :sd 0 :wd 0} :letter %)
-                     "REIMAGINE"))
-
-        update-letter
-        (fn [letter-data]
-          (-> letter-data
-              (assoc :ad (new-delta :ad))
-              (assoc :sd (new-delta :sd))
-              (assoc :wd (new-delta :wd))))]
+  (let [letters "REIMAGINE"]
     (fn []
-      (js/setTimeout
-       (fn []
-         (swap! letters #(map update-letter %)))
-       10000) ;; change spinnies every 10s
       (into
        [:div {:id "logo"}]
-       (for [letter-data @letters]
+       (for [letter-data letters]
          [letter-component letter-data])))))
 
 (defn toggle-fullscreen []
