41bc81a3328bef89ec1357385090f811135af57f
Ben Swift <ben@benswift.me>
2019-05-02 11:17:22 +1000

    finally got the animation stuff (pretty much) working again

--- _cljs/reimagine_logo/src/reimagine_logo/core.cljs
+++ _cljs/reimagine_logo/src/reimagine_logo/core.cljs
@@ -10,36 +10,36 @@
         (case attribute
           :angle [-3600 3600]
           :size [5 25]
-          :weight [100 900])])
-  (+ min (rand (- max min))))
+          :weight [100 900])]
+    (+ min (rand (- max min)))))
 
-(defn attribute->style [attribute value]
+(defn init-animation-params [attribute]
+  {:initial (rand-attribute-val attribute)
+   :final (rand-attribute-val attribute)
+   :num-steps (rand-int 60)})
+
+(defn value->css [attribute value]
   (case attribute
     :angle {:transform (gstring/format "rotate(%.2fdeg)" value)}
     :size {:font-size (gstring/format "%.2fvmin" value)}
     :weight {:font-weight value}))
 
-(defn easing-closure [attribute initial]
-  (let [final (rand-attribute-val attribute)
-        range (- final initial)
-        num-steps (rand-int 60)]
-    (fn [step]
-      (->> (/ step num-steps)
-           (ease-in-out transition-elastic)
-           (* range)
-           (+ initial)))))
+(defn easing-fn [step {:keys [initial final num-steps]}]
+   (->> (/ step num-steps)
+        (ease-in-out transition-elastic)
+        (* (- final initial))
+        (+ initial)))
 
 (defn letter-component [letter]
   (let [step (r/atom 0)
-        style-transition-closures
-        (into {} (map (fn [attr] {attr (easing-closure attr (rand-attribute-val attr))}) [:angle :size :weight]))]
-    (fn [letter]
+        animation-params
+        (map (juxt identity init-animation-params) [:angle :size :weight])]
+    (fn []
       (js/setTimeout #(swap! step inc) (/ 1000 60)) ;; 60fps
       ^{:key letter}
       [:div.letter
        {:style
-        (into {} (map (fn [[attr easing-fn]] {attr (attribute->style attr (easing-fn 0))})
-                      style-transition-closures))}
+        (into {} (map (fn [[attr anim-params]] (value->css attr (easing-fn @step anim-params))) animation-params))}
        letter])))
 
 (defn logo-component []
