e510cdd08c81755784a7abfb5f197f968ac726d1
Ben Swift <ben@benswift.me>
2019-12-13 15:17:43 +0800

    add npm stuff + browserify to make it work in the browser

---
+++ _plugins/browserify.rb
@@ -0,0 +1,11 @@
+# run browserify to package all the things
+
+def browserify_all(site)
+  Dir.glob("_browserify/*.js") do |filename|
+    system("npx", "browserify", filename, "--outfile", "assets/js/#{File.basename(filename)}") or raise "browserify build error in #{filename}"
+  end
+end
+
+Jekyll::Hooks.register :site, :pre_render do |site|
+  browserify_all site
+end
