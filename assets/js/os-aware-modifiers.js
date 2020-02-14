01c7598416461602d9c652eaab998adefa7133b6
Ben Swift <ben@benswift.me>
2020-02-14 19:58:20 +0000

    make script a bit nicer

--- assets/js/os-aware-modifiers.js
+++ assets/js/os-aware-modifiers.js
@@ -1,9 +1,15 @@
-let modKeys = ["control", "command", "ctrl", "cmd"];
+// see https://benswift.me/blog/2020/02/14/command-control-giving-os-aware-keybinding-hints/
 
-let modifier = navigator.appVersion.indexOf("Mac")!=-1 ? "⌘" : "CTRL";
+window.addEventListener('DOMContentLoaded', (event) => {
 
-for (const kbdElement of document.querySelectorAll("kbd")) {
-  if (modKeys.includes(kbdElement.textContent.toLowerCase()) && !kbdElement.classList.contains("nopretty")){
-	kbdElement.textContent = modifier;
+  // replace all these with the appropriate modifier for the platform
+  let modKeys = ["control", "command", "ctrl", "ctl", "cmd"];
+
+  let modifier = navigator.appVersion.indexOf("Mac")!=-1 ? "⌘" : "CTRL";
+
+  for (const kbdElement of document.querySelectorAll("kbd")) {
+	if (modKeys.includes(kbdElement.textContent.toLowerCase()) && !kbdElement.classList.contains("nopretty")){
+	  kbdElement.textContent = modifier;
+	}
   }
-}
+});
