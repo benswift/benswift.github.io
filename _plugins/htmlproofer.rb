c2731eaa2041ba6b0188b1c1405d2931748d3b6e
Ben Swift <ben@benswift.me>
2021-03-07 20:22:02 +1100

    improved HTML checking with nokogumbo (via html-proofer)

--- _plugins/htmlproofer.rb
+++ _plugins/htmlproofer.rb
@@ -3,9 +3,15 @@ require "html-proofer"
 Jekyll::Hooks.register :site, :post_write do |site|
   HTMLProofer.check_directory(site.config["destination"], opts = {
                                 :check_html => true,
+                                :validation => {
+                                  :report_eof_tags => true,
+                                  :report_invalid_tags => true,
+                                  :report_mismatched_tags => true,
+                                  :report_missing_doctype => true,
+                                  :report_missing_names => true
+                                },
                                 :check_img_http => true,
                                 :disable_external => true, # change to false for a thorough check, but takes ages
-                                :report_invalid_tags => true,
                                 :file_ignore => [/assets\/reveal.js-3.8.0\//]
                               }).run
 end
