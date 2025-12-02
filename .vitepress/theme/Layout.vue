509449fd2e7e0074819d18eb47ec929bc08bbcc9
Ben Swift <ben@benswift.me>
2025-12-02 14:32:37 +1100

    Add custom footer with Iconify icons and cafe easter egg

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -2,12 +2,17 @@
 import { useData } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import RevealLayout from "./layouts/RevealLayout.vue";
+import SiteFooter from "./components/SiteFooter.vue";
 
 const { Layout } = DefaultTheme;
 const { frontmatter } = useData();
 </script>
 
 <template>
-  <RevealLayout v-if="frontmatter.layout === 'reveal'" />
-  <Layout v-else />
+    <RevealLayout v-if="frontmatter.layout === 'reveal'" />
+    <Layout v-else>
+        <template #doc-footer-before>
+            <SiteFooter />
+        </template>
+    </Layout>
 </template>
