8521dc24dcf626c31c2d87fdebc4a6e0df58c2c0
Ben Swift <ben@benswift.me>
2026-03-02 21:21:59 +1100

    remove vitepress cache from repo, add to gitignore

--- .vitepress/cache/deps/three_addons_lines_LineGeometry__js.js
+++
@@ -1,9 +0,0 @@
-import {
-  LineGeometry
-} from "./chunk-N3JFNIQ3.js";
-import "./chunk-SQDRCFMA.js";
-import "./chunk-V6TY7KAL.js";
-export {
-  LineGeometry
-};
-//# sourceMappingURL=three_addons_lines_LineGeometry__js.js.map
--- .vitepress/cache/deps/three_addons_lines_LineMaterial__js.js
+++
@@ -1,9 +0,0 @@
-import {
-  LineMaterial
-} from "./chunk-EOJLOIW2.js";
-import "./chunk-SQDRCFMA.js";
-import "./chunk-V6TY7KAL.js";
-export {
-  LineMaterial
-};
-//# sourceMappingURL=three_addons_lines_LineMaterial__js.js.map
