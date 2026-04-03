73f4927858886758239c54957cbceb431645b6fc
Ben Swift <ben@benswift.me>
2026-04-04 09:59:01 +1100

    run feed tests as post-build integration tests in CI

--- vitest.integration.config.ts
+++ vitest.integration.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   test: {
-    include: ["**/*.test.ts"],
-    exclude: ["node_modules/**", "**/*.integration.test.ts"],
+    include: ["**/*.integration.test.ts"],
+    exclude: ["node_modules/**"],
   },
 })
